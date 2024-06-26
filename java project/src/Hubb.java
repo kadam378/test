ad095747752b12cc1a05dd2cfc06948eaa130e92
sdac-5 <sdac-5@sdac-5-PC>
2024-06-26 14:36:27 -0400

    main project

---
+++ java project/src/Hubb.java
@@ -0,0 +1,14 @@
+ class Hubb{
+ 
+	void meth(int a){
+		System.out.println(a);
+	}
+	void meth(int a, int b){
+		System.out.println(a+b);
+	}
+	public static void main(String[] args) {
+		Hubb h = new Hubb();
+		h.meth(3);
+		h.meth(3,4);
+	}
+}
